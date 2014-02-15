ff3a03ca55910003ae33f918fada8f7e67ca75db
Andreas Sotnik <andreas.sotnik@gmail.com>
2014-02-15 11:51:44 -0500

    updates

--- public/js/app.js
+++ public/js/app.js
@@ -4,24 +4,32 @@ var zenturioEditor = angular.module('zenturio-editor', []);
 
 $(function() {
   $(document).foundation();
+  if ($.cookie('info_block_hidden') == "yes") {
+    toggleInfoBlock();
+  }
 });
 
 function toggleInfoBlock() {
   $('.left-side').toggleClass('large-8');
   $('.left-side').toggleClass('large-12');
 
+  $('.right-side').toggleClass('hidden');
   $('.right-side').toggle();
+
+  $.cookie('info_block_hidden', $('.right-side').hasClass('hidden') ? "yes" : "no");
 }
 
-var loadedIds =  [];
+var loadedIds = [];
 
 function loadTree(id, subpath) {
   if (loadedIds[id]) {
-    $('#rowModelFiles'+id).toggle();
+    $('#rowModelFiles' + id).toggle();
   } else {
-    $.get('/tree-inner', {subpath:subpath}, function(data) {
-      $('#rowModelFiles'+id+" .inner-dir").html(data);
-      $('#rowModelFiles'+id).show();
+    $.get('/tree-inner', {
+      subpath: subpath
+    }, function(data) {
+      $('#rowModelFiles' + id + " .inner-dir").html(data);
+      $('#rowModelFiles' + id).show();
       loadedIds[id] = true;
     });
   }
