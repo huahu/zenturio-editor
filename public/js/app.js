af4d7c2ae0a40027d29efec2d588adb251faba50
Andreas Sotnik <andreas.sotnik@gmail.com>
2014-02-15 12:12:41 -0500

    remove angularJs dependency

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,3 @@
-var zenturioEditor = angular.module('zenturio-editor', []);
-
-// todo: wite angular controllers here
-
 $(function() {
   $(document).foundation();
   if ($.cookie('info_block_hidden') == "yes") {
