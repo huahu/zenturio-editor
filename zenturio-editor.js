2ee19f4f6ca0f090e9a8a19f52a5b3f0a5c1b194
Andreas Sotnik <andreas.sotnik@gmail.com>
2014-02-15 07:45:42 -0500

    basic ejs

--- zenturio-editor.js
+++ zenturio-editor.js
@@ -1,3 +1,23 @@
 #!/usr/bin/env node
 
-// todo: write something
+var express = require('express');
+var fs = require('fs');
+var app = express();
+app.set('view engine', 'ejs');
+
+app.use('/', express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+  res.render('tree');
+});
+
+app.get('/editor', function(req, res) {
+  res.render('editor');
+});
+
+var port = 3000;
+app.listen(port, function(err) {
+  if (!err) {
+    console.log('Zenturio Editor started at port ', port);
+  }
+});
